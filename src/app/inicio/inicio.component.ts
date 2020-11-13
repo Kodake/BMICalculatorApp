12f558fbe6c2f0b5644319c0bd370f13e675fe48
Kodake <39713700+Kodake@users.noreply.github.com>
2020-11-13 03:21:57 -0400

    All changes about BmiCalculatorApp

---
+++ src/app/inicio/inicio.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.component.html',
+  styleUrls: ['./inicio.component.css']
+})
+export class InicioComponent implements OnInit {
+
+  edad: number = 25;
+  peso: number = 60;
+  altura:number = 185;
+  sexo: string = 'Masculino';
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  cambiarAltura(event: any): void {
+    this.altura = event.target.value;
+  }
+
+  masculino(): void {
+    this.sexo = 'Masculino';
+  }
+
+  femenino(): void {
+    this.sexo = 'Femenino';
+  }
+
+  calcular(): void {
+    const BMI = this.peso / Math.pow(this.altura / 100, 2);
+    this.router.navigate(['resultado', BMI.toFixed(1)]);
+  }
+
+}
